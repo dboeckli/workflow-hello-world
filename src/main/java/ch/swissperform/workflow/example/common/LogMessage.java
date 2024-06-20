b0ca867e58b729470d1546c9ba85fe417f6e028d
boeckli@swissperform.ch <boeckli@swissperform.ch>
2024-06-20 15:58:04 +0200

    reformat

--- src/main/java/ch/swissperform/workflow/example/common/LogMessage.java
+++ src/main/java/ch/swissperform/workflow/example/common/LogMessage.java
@@ -5,16 +5,12 @@ import org.slf4j.helpers.MessageFormatter;
 public enum LogMessage {
 
     // System
-    READING_CONFIG_CLASS                            (10001, "[{}] Reading configuration from {} config map: {}"),
-    RECEIVED_REFRESH_EVENT_FOR_CONFIG_MAP           (10002, "[{}] Received Refresh event for Config Map configuration: {}"),
-    RECEIVED_CONTEXT_REFRESH_EVENT                  (10003, "[{}] ### Environment and configuration ###"),
-    HTTP_REQUEST                                    (10004, "[{}] ### [uid={}] {} {} url={}{}"),
-    HTTP_RESPONSE                                   (10005, "[{}] ### [uid={}] {} httpCode={}, message={}"),
+    READING_CONFIG_CLASS(10001, "[{}] Reading configuration from {} config map: {}"), RECEIVED_REFRESH_EVENT_FOR_CONFIG_MAP(10002, "[{}] Received Refresh event for Config Map configuration: {}"), RECEIVED_CONTEXT_REFRESH_EVENT(10003, "[{}] ### Environment and configuration ###"), HTTP_REQUEST(10004, "[{}] ### [uid={}] {} {} url={}{}"), HTTP_RESPONSE(10005, "[{}] ### [uid={}] {} httpCode={}, message={}"),
 
-    JACKSON_CONFIG_START                            (10006, "[{}] ### JacksonDataFormatConfigurator configure started."),
-    JACKSON_CONFIG_MODULES                          (10007, "[{}] ### Registered module: {}, version: {}");
+    JACKSON_CONFIG_START(10006, "[{}] ### JacksonDataFormatConfigurator configure started."), JACKSON_CONFIG_MODULES(10007, "[{}] ### Registered module: {}, version: {}");
 
     private final int id;
+
     private final String format;
 
     LogMessage(final int id, final String format) {
@@ -22,11 +18,13 @@ public enum LogMessage {
         this.id = id;
     }
 
-    public String formatMessage(final Object ... arguments) {
-        return MessageFormatter.arrayFormat(getMessage(), arguments).getMessage();
+    public String formatMessage(final Object... arguments) {
+        return MessageFormatter.arrayFormat(getMessage(), arguments)
+                               .getMessage();
     }
 
     public String getMessage() {
-        return MessageFormatter.format(format, "logid=" + id).getMessage();
+        return MessageFormatter.format(format, "logid=" + id)
+                               .getMessage();
     }
 }
