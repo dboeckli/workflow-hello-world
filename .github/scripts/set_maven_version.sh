f2b16811d7a8c8ef573e344cb499e2550ef5fcfa
boeckli@swissperform.ch <boeckli@swissperform.ch>
2024-06-25 14:27:07 +0200

    adapt build for sonar and prepare deployment

--- .github/scripts/set_maven_version.sh
+++ .github/scripts/set_maven_version.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
+set -o pipefail    # don't hide errors within pipes
+set -o nounset     # abort on unbound variable
+set -o errexit     # abort on nonzero exit status
+
 echo "### Will change the version in pom.xml files..."
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "### Current git branch is $branch"
 
-if [ $branch == "main" ]; then
-  echo "### Main branch is checked out. Aborting..."
-  exit;
+# get current branch name
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "### Branch is $BRANCH_NAME"
+branch=${BRANCH_NAME//[\/]/_}
+echo "### Branch is $branch"
+echo "BRANCH_NAME=$branch" >> "$GITHUB_OUTPUT"
+
+# get current version of the top level pom
+# TODO: for future we want to use the default artifactory profile. here it is deactivated by intention, needs some research.
+mvn help:evaluate -e -P '!artifactory' -Dexpression=project.version
+MVN_VERSION=$( mvn help:evaluate -P '!artifactory' -Dexpression=project.version -q -DforceStdout )
+echo "MVN_VERSION=$MVN_VERSION" >> "$GITHUB_OUTPUT"
+echo "### Current version is: $MVN_VERSION"
+
+# extract version suffix
+suffix=$(echo "$MVN_VERSION" | cut -d \- -f 2)
+echo "### Suffix is: $suffix"
+
+prefix=$(echo "$MVN_VERSION" | cut -d \- -f 1)
+echo "### Prefix is: $prefix"
+
+# build new version
+if [[ "$branch" != "master" ]] && [[ "$branch" != "main" ]]
+then
+    NEW_MAVEN_VERSION=${prefix}_${branch}-${suffix}
+else
+    NEW_MAVEN_VERSION=$MVN_VERSION
 fi
 
-branch=${branch//[\/]/_}
-
-MAVEN_VERSION=$( mvn help:evaluate -P '!artifactory' -Dexpression=project.version -q -DforceStdout )
-echo "### Current maven project version is: $MAVEN_VERSION"
-
-prefix=$(echo $MAVEN_VERSION | cut -d \- -f 1)
-echo "### Version Prefix is: $prefix"
-
-suffix=$(echo $MAVEN_VERSION | cut -d \- -f 2)
-echo "### Version suffix is: $suffix"
-
-BRANCH_MAVEN_VERSION=${branch}_$prefix-$suffix
-
-echo "### Branch maven version is: $BRANCH_MAVEN_VERSION"
-echo "BRANCH_MAVEN_VERSION=$BRANCH_MAVEN_VERSION" >> "$GITHUB_OUTPUT"
-echo "GIT_BRANCH=$GIT_BRANCH" >> "$GITHUB_OUTPUT"
-echo "ORIGINAL_MAVEN_VERSION=$ORIGINAL_MAVEN_VERSION" >> "$GITHUB_OUTPUT"
+echo "BRANCH_MVN_VERSION=$NEW_MAVEN_VERSION" >> "$GITHUB_OUTPUT"
+echo "### Changed version in pom.xml files $NEW_MAVEN_VERSION"
 
 
---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "[INFO] not implemented yet"
