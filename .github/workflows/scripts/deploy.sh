20c845a9b256483e3d6e5ee9bb5b8f8b9c120fb2
boeckli@swissperform.ch <boeckli@swissperform.ch>
2024-06-26 15:42:03 +0200

    implement deployment

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -129,8 +129,8 @@ function deploy() {
     echo "[INFO] ### deploying ${ARTIFACT_ID}-${BRANCH_MVN_VERSION}.jar to ${RUNTIME_DIR}/${ARTIFACT_ID}.jar"
     sudo cp "${ACTION_RUNNER_DEPLOYMENT_WORKING_DIR}/${ARTIFACT_ID}-${BRANCH_MVN_VERSION}.jar" "${RUNTIME_DIR}/${ARTIFACT_ID}.jar"
     
-    sudo chown "${RUNTIME_USER}:${RUNTIME_USER}" "${RUNTIME_DIR}/${ARTIFACT_ID}-${BRANCH_MVN_VERSION}.jar"
-    sudo chmod 500 "${RUNTIME_DIR}/${ARTIFACT_ID}-${BRANCH_MVN_VERSION}.jar"
+    sudo chown "${RUNTIME_USER}:${RUNTIME_USER}" "${RUNTIME_DIR}/${ARTIFACT_ID}.jar"
+    sudo chmod 500 "${RUNTIME_DIR}/${ARTIFACT_ID}.jar"
 
     sleep 15
   else
