f5c944ad859769498c18199f7bdd291a3c3ee884
boeckli@swissperform.ch <boeckli@swissperform.ch>
2024-06-27 11:34:34 +0200

    expose camunda openapi

---
+++ src/main/java/ch/swissperform/workflow/example/controller/CamundaApiRestController.java
@@ -0,0 +1,35 @@
+package ch.swissperform.workflow.example.controller;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import lombok.Builder;
+import org.apache.commons.io.IOUtils;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+@RestController
+@RequestMapping(value = "/restapi/camunda", produces = "application/json")
+public class CamundaApiRestController {
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public String getFile() {
+        InputStream inputStream = getClass().getResourceAsStream("/openapi.json");
+        try {
+            return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            Response response = Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+            throw new WebApplicationException(response);
+        }
+    }
+}
