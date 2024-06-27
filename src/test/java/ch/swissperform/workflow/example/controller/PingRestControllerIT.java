f6e1f4fc95fbb1113687af81a723f91360ac602a
boeckli@swissperform.ch <boeckli@swissperform.ch>
2024-06-27 13:45:36 +0200

    add more unit test

--- src/test/java/ch/swissperform/workflow/example/controller/PingRestControllerIT.java
+++ src/test/java/ch/swissperform/workflow/example/controller/PingRestControllerIT.java
@@ -7,7 +7,6 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DirtiesContext
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class PingRestControllerIT {
 
     @Autowired
