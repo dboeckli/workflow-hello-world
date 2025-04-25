8e57689ff4f2126a7ce65f932234c164d7dd7413
adm-boeckli <dboeckli@gmail.com>
2025-04-25 12:11:16 +0200

    fix build

--- src/test/java/ch/bpm/workflow/example/controller/CamundaOpenApiRestControllerIT.java
+++ src/test/java/ch/bpm/workflow/example/controller/CamundaOpenApiRestControllerIT.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles(value = "local")
+@DirtiesContext
 @Slf4j
 class CamundaOpenApiRestControllerIT {
 
