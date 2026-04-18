e522b70c053b711646fd0167bf39a6375bef2abe
Dominique Boeckli <dboeckli@gmail.com>
2026-04-18 09:25:56 +0200

    fix helm tests 2

--- .run/scripts/test-k8s.ps1
+++ .run/scripts/test-k8s.ps1
@@ -2,4 +2,4 @@ cd target/helm/repo
 
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 Write-Host "test application: $APPLICATION_NAME"
-helm test $APPLICATION_NAME --namespace workflow-hello-world --logs
+helm test $APPLICATION_NAME --namespace workflow-hello-world
